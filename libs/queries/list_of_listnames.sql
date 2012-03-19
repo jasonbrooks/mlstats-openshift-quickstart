30d47f11d0a5d02ce0c26e09e624fc3718cbb98b
Jason Brooks <jason@sealrock.net>
2012-03-19 11:37:59 -0700

    tweak scripts

--- libs/queries/list_of_listnames.sql
+++ libs/queries/list_of_listnames.sql
@@ -1,1 +1,1 @@
-select distinct(substring_index(TRIM(TRAILING '/' FROM mailing_list_url), '/', -1)) list_name, substring_index(substring_index(TRIM(TRAILING '/' FROM mailing_list_url), '.', -2), '/', 1) domain from messages
+select distinct(substring_index(TRIM(TRAILING '/' FROM mailing_list_url), '/', -1)) list_name, substring_index(substring_index(TRIM(TRAILING '/' FROM mailing_list_url), '.', -2), '/', 1) domain from messages order by list_name
