6c0fc4b912e00c18d83b017c8a9c034a263005d2
Jason Brooks <jason@sealrock.net>
2012-03-20 23:21:27 -0700

    hmm

--- libs/queries/list_of_dates.sql
+++ libs/queries/list_of_dates.sql
@@ -1,4 +1,1 @@
-select current_timestamp() into @endperiod;
-select first_date into @startperiod from messages where left(first_date, 4) > 1979 order by first_date limit 1;
-call make_intervals(@startperiod,@endperiod,1,'DAY');
-select left(interval_start, 10) Date from time_intervals;
+SELECT list_all.fdl Date FROM (select LEFT(first_date, 10) fdl from messages where LEFT(first_date, 4) > 1979 group by fdl order by first_date) list_all
