62092399254ade4598f30ad4f6ee08daea4062a0
Jason Brooks <jason@sealrock.net>
2012-03-20 23:12:42 -0700

    test the now more dateful date list

--- libs/queries/list_of_dates.sql
+++ libs/queries/list_of_dates.sql
@@ -1,1 +1,4 @@
-select left(interval_start, 10) Date from time_intervals
+select current_timestamp() into @endperiod;
+select first_date into @startperiod from messages where left(first_date, 4) > 1979 order by first_date limit 1;
+call make_intervals(@startperiod,@endperiod,1,'DAY');
+select left(interval_start, 10) Date from time_intervals;
